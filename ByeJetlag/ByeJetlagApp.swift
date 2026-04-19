7f77a55ac397514594e2622d45be755ba6ebe47f
cindy <kinvictorwin@gmail.com>
2026-04-20 04:34:48 +0700

    Initial Commit

---
+++ ByeJetlag/ByeJetlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByeJetlagApp.swift
+//  ByeJetlag
+//
+//  Created by Cindy Kin on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ByeJetlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
